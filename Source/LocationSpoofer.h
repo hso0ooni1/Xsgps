50a539465970b9ec5d0c26cf475053d1a32fa7cf
hso0ooni1 <101964528+hso0ooni1@users.noreply.github.com>
2026-09-20 12:49:57 +0300

    Add one-shot location refresh API

--- Source/LocationSpoofer.h
+++ Source/LocationSpoofer.h
@@ -6,6 +6,7 @@ NS_ASSUME_NONNULL_BEGIN
 FOUNDATION_EXPORT CLLocation *LSCreateSpoofedLocation(void);
 FOUNDATION_EXPORT BOOL LSIsInternalLocationCreate(void);
 FOUNDATION_EXPORT void LSSetHooksBypassed(BOOL bypassed);
+FOUNDATION_EXPORT void LSNotifySpoofLocationChanged(void);
 
 @interface LocationSpoofer : NSObject
 
