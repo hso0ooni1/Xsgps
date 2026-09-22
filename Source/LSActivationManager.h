6dfbb82de9eeb1f38291d04da1308fbf33d57e6a
hso0ooni1 <101964528+hso0ooni1@users.noreply.github.com>
2026-09-22 21:43:44 +0300

    Expose transferable XsGpS app UUID API

--- Source/LSActivationManager.h
+++ Source/LSActivationManager.h
@@ -3,6 +3,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 typedef void (^LSActivationCompletion)(BOOL success, NSString *message);
+typedef void (^LSIdentityTransferCompletion)(BOOL success, NSString *message, NSString * _Nullable transferCode);
 
 @interface LSActivationManager : NSObject
 
@@ -10,10 +11,15 @@ typedef void (^LSActivationCompletion)(BOOL success, NSString *message);
 
 @property (nonatomic, readonly, getter=isActivated) BOOL activated;
 @property (nonatomic, copy, readonly) NSString *deviceID;
+@property (nonatomic, copy, readonly) NSString *installationUUID;
 @property (nonatomic, copy, readonly, nullable) NSString *activationCode;
 @property (nonatomic, strong, readonly, nullable) NSDate *expiresAt;
 
 - (void)warmUp;
+/// The current activated device generates a one-time ten-minute transfer code.
+- (void)prepareIdentityTransferWithCompletion:(LSIdentityTransferCompletion)completion;
+/// The new installation imports its previous XsGpS identity after one-time verification.
+- (void)completeIdentityTransferFromUUID:(NSString *)oldUUID transferCode:(NSString *)transferCode completion:(LSActivationCompletion)completion;
 - (void)activateCode:(NSString *)code completion:(LSActivationCompletion)completion;
 - (void)verifyNowWithCompletion:(nullable LSActivationCompletion)completion;
 
